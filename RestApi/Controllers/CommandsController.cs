0b820d7c22eb7c0172be39c9fb82c0f350dd1627
com.Kondratov <com.Kondratov@gmail.com>
2020-10-04 08:50:36 +0300

    Добавил атрибуты в Dto

--- RestApi/Controllers/CommandsController.cs
+++ RestApi/Controllers/CommandsController.cs
@@ -56,7 +56,6 @@ namespace RestApi.Controllers
             var commandReadDto = _mapper.Map<CommandReadDto>(commandModel);
 
             return CreatedAtRoute(nameof(GetCommandById), new { Id = commandReadDto.Id }, commandReadDto);
-            //return Ok(commandReadDto);
         }
 
     }
